2d7beac6c44c095c485c4d9ab3e5dc2f1a49e9da
VincentRPS <vincentbusiness55@gmail.com>
2022-08-19 13:37:17 +0800

    fix: auto-deploy name

--- auto-deploy.sh
+++ auto-deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo docker build -t derailed-gateway .
-sudo docker run -p 5000:5000 -d derailed-gateway
+sudo docker build -t derailed-api .
+sudo docker run -p 5000:5000 -d derailed-api
