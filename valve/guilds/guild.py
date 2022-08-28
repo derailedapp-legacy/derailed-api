91c196394b446c3bac290d1120a804a62e74186a
VincentRPS <vincentbusiness55@gmail.com>
2022-08-28 20:52:23 +0800

    feat: get guild and get guild preview

--- valve/guilds/guild.py
+++ valve/guilds/guild.py
@@ -45,6 +45,40 @@ async def create_guild(
     return guild.dict()
 
 
+@router.get('/{guild_id}', status_code=200)
+async def get_guild(guild_id: str, user: User | None = Depends(get_user)) -> dict:
+    if user is None:
+        raise NoAuthorizationError()
+
+    is_member = await Member.find_one(Member.user_id == user.id, Member.guild_id == guild_id).exists()
+
+    if is_member is False:
+        raise HTTPException(403, 'You are not a member of this guild')
+
+    guild = await Guild.find_one(Guild.id == guild_id)
+    return guild.dict()
+
+
+@router.get('/{guild_id}/preview', status_code=200)
+async def get_guild_preview(guild_id: str, user: User | None = Depends(get_user)) -> dict:
+    if user is None:
+        raise NoAuthorizationError()
+
+    is_member = await Member.find_one(Member.user_id == user.id, Member.guild_id == guild_id).exists()
+
+    if is_member is False:
+        raise HTTPException(403, 'You are not a member of this guild')
+
+    guild = await Guild.find_one(Guild.id == guild_id)
+    guildd = guild.dict()
+
+    guildd['member_count'] = await Member.find(Member.guild_id == guild_id).count()
+    # TODO: Actually find a way to count this
+    guildd['online_count'] = 0
+
+    return guild.dict()
+
+
 @router.patch('/{guild_id}', status_code=200)
 async def modify_guild(
     guild_id: str, model: ModifyGuild, user: User | None = Depends(get_user)
