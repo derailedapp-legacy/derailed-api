ec23d9895f9ba9d5b0d77f0c1ab5495b8b72e33a
Vincent <vincentbusiness55@gmail.com>
2022-09-11 14:26:05 +0800

    chore: remove ``

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
 
-exec gunicorn \
-    -w $((`nproc` * 2 + 1)) \
-    -k "uvicorn.workers.UvicornWorker" \
-    -b "0.0.0.0:5000" \
-    "app:app"
+exec gunicorn -w $((`nproc` * 2 + 1)) -k "uvicorn.workers.UvicornWorker" -b "0.0.0.0:5000" "app:app"
